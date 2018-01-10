0482866433d30923d67852212f7c8a66c60d5af6
manuelblanch <manuelblanch@iesebre.com>
2018-01-10 18:20:26 +0100

    new

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,10 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+      $tasks = Task::where(['user_id' => Auth::user()->id])->get();
+      return response()->json([
+          'tasks'    => $tasks,
+      ], 200);
     }
 
     /**
@@ -34,7 +37,7 @@ class TaskController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    
+
         //    public function store(Request $request)
     {
         $this->validate($request, [
