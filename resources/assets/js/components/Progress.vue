6302de45f6b23e5fd929a96f2addf3f079eed3d6
manuelblanch <manuelblanch@iesebre.com>
2018-01-25 19:06:33 +0100

    hello

---
+++ resources/assets/js/components/Progress.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="progress" :class="[progressSize, isVertical?'vertical':'', isActive?'active':'']">
+    <div class="progress-bar"
+         :class="[`progress-bar-${type}`, (isStriped?'progress-bar-striped':'')]"
+         role="progressbar"
+         :aria-valuenow="value"
+         :aria-valuemin="min"
+         :aria-valuemax="max"
+         :style="`${isVertical?'height':'width'}: ${value}%`">
+      <span class="sr-only">{{text}}</span>
+    </div>
+  </div>
+
+</template>
