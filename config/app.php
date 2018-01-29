228bef98e9e2f7e87ddeec270643c795dd67915f
manuelblanch <manuelblanch@iesebre.com>
2018-01-29 20:03:45 +0100

    adding

--- config/app.php
+++ config/app.php
@@ -225,6 +225,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Html' => Spatie\Menu\Laravel\Html::class,
+        'Link' => Spatie\Menu\Laravel\Link::class,
+        'Menu' => Spatie\Menu\Laravel\Menu::class,
 
     ],
 
