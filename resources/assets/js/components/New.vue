4ffbea4f5019481af5f6bfb41c91a8d0d9e40a72
manuelblanch <manuelblanch@iesebre.com>
2018-02-05 19:31:24 +0100

    adding

---
+++ resources/assets/js/components/New.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="new">
+        <button v-on:click="toggleText()">ToggleText</button>
+        <p v-if="showText">{{ text }}</p>
+        <p>{{ reverseText }}</p>
+        <hr />
+        <ul>
+            <li v-for="user in users" v-on:click="showUser()">
+                {{ user }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: '',
+                showText: false,
+                users: ['1', '2', '3', '4']
+            }
+        },
+        methods: {
+            toggleText() {
+                this.showText = !this.showText;
+            },
+            showUser(user) {
+                console.log(user);
+            }
+        }
+    }
+</script>
