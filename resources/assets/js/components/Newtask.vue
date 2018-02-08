de97db3ed358902b691b7bac3aca80d68576535f
manuelblanch <manuelblanch@iesebre.com>
2018-02-08 19:50:38 +0100

    adding

---
+++ resources/assets/js/components/Newtask.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="new">
+        <button v-on:click="toggleText()">ToggleText</button>
+        <p v-if="showText">{{ text }}</p>
+        <p>{{ reverseText }}</p>
+        <hr />
+        <ul>
+            <li v-for="user in users" v-on:click="showUser()">
+                {{ user }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: '',
+                showText: false,
+                users: ['Task1', 'Task2', 'Task3']
+            }
+        },
+        methods: {
+            toggleText() {
+                this.showText = !this.showText;
+            },
+            showUser(user) {
+                console.log(user);
+            }
+        }
+    }
+</script>
